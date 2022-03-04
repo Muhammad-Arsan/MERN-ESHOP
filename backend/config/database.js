226700bed836966bbfb9edee71399a743b44bd2d
Muhammad Arsan <muhammad.arsan@optimusfox.com>
2022-03-04 17:59:59 +0500

    api features

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,32 @@
+//const dotenv = require("dotenv");
+// const mongoose = require("mongoose");
+
+// const connectDatabase = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("MOngo DB connection Success");
+//   } catch (error) {
+//     console.error("MONGO DB Conection FAIL");
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDatabase;
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
+  console.log("mongo Connect .....", process.env.MONGO_URI);
   mongoose
-    .connect(process.env.DB_URI, {
+    .connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
     })
     .then((data) => {
       console.log(`MongoDB connected with server: ${data.connection.host}`);
-    })
-    .catch((err) => {
-      console.log(err);
     });
 };
 module.exports = connectDatabase;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,31 @@
 const app = require("./app");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const connectDatabase = require("./config/database");
+
+//Handlling uncahgt exception
+
+process.on("uncaughtException", (err) => {
+  console.log(`Error :${err.message}`);
+  console.log("Shutting Dwon the server due to uncahgt exception");
+  process.exit(1);
+});
 //config
 
-dotenv.config({ path: "backend/config/config.env" });
+// dotenv.config({ path: "backend/config/config.env" });
 
+//conecting database
 connectDatabase();
-app.listen(process.env.PORT, () => {
+
+const server = app.listen(process.env.PORT, () => {
   console.log(`server is running on http://localhost:${process.env.PORT}`);
+});
+
+//Unhandled Promise Rejection
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error : ${err.message}`);
+  console.log("ShuttingDown the server due to Unhandled Promise Rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
