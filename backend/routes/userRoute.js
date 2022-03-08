85dccaf8decbe7b967c8ec30a960bb334f07e9f0
Muhammad-Arsan <arsan924@gmail.com>
2022-03-09 00:21:14 +0500

    dfds

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,6 +5,9 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  getUserDetails,
+  updatePassword,
+  updateProfile,
 } = require("../controllers/userController");
 const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
@@ -14,5 +17,17 @@ router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
-router.route("/me").get(isAuthenticatedUser, getUser);
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUser);
+
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
+
 module.exports = router;
