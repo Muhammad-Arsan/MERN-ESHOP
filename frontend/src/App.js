3715d33a4ddec6911af82ac03204d2e3bd461b9e
Muhammad-Arsan <arsan924@gmail.com>
2022-03-22 22:59:18 +0500

    Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Header from "./component/layout/Header/Header.js";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./component/layout/Footer/Footer";
+import Home from "./component/Home/Home";
 function App() {
   return (
     <Router>
       <Header />
-
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </Router>
   );
---
+++ frontend/src/component/Home/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { CgMouse } from "react-icons/all";
+import "./Home.css";
+import Product from "./Product.js";
+import MetaData from "../layout/MetaData";
+const Home = () => {
+  const product = {
+    name: "Product1",
+    price: "200",
+    images: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
+    _id: "arsan",
+  };
+  return (
+    <>
+      <MetaData title="Home Page" />
+      <div className="banner">
+        <p>Welcome to Ecommerce</p>
+        <h1>FIND AMAZING PRODUCTS BELOW</h1>
+        <a href="#container">
+          <button>
+            Scroll <CgMouse />
+          </button>
+        </a>
+      </div>
+      <h2 className="homeHeading">Featured Products</h2>
+      <div className="container" id="container">
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+      </div>
+    </>
+  );
+};
+
+export default Home;
